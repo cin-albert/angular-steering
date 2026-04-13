6d90c8104070aa09565fa4d3d90b0c2a6fb66573
cin-albert <albert@cinnamon.is>
2026-04-14 00:06:20 +0700

    feat: add purified configs p9-p19

--- vllm_angular_steering.sh
+++ vllm_angular_steering.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DATASETS=(
-    "math500"
+    # "math500"
+    "tinymath500"
     # "livecodebench"
     # "arc"
     # "tinylivecodebench"
@@ -46,7 +47,7 @@ SCENARIOS_TO_CONFIG_FILES=(
     ["S8-DeepSeek-R1-Distill-Qwen-14B-livecodebench"]="steering_configs/DeepSeek-R1-Distill-Qwen-14B/steering_config-s8-pca_0-livecodebench.npy"
     ["S8-DeepSeek-R1-Distill-Qwen-14B-tinylivecodebench"]="steering_configs/DeepSeek-R1-Distill-Qwen-14B/steering_config-s8-pca_0-livecodebench.npy"
     ["S8-DeepSeek-R1-Distill-Qwen-14B-arc"]="steering_configs/DeepSeek-R1-Distill-Qwen-14B/steering_config-s8-pca_0-arc.npy"
-    ["S8-DeepSeek-R1-Distill-Qwen-14B-math500-purified"]="steering_configs/DeepSeek-R1-Distill-Qwen-14B/purified/steering_config-pca_0-math500-purified.npy"
+    ["S8-DeepSeek-R1-Distill-Qwen-14B-math500-purified"]="steering_configs/DeepSeek-R1-Distill-Qwen-14B/purified/steering_config-pca_0-math500-purified-p9-p19.npy"
 
 
     # Qwen3-14B
